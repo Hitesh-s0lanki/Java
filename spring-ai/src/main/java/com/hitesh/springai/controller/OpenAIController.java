2c4afb0ba938987e5313730ae9db1453e2ae305b
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-05-01 15:20:06 +0530

    -spring ai for chat completion - openai

--- spring-ai/src/main/java/com/hitesh/springai/controller/OpenAIController.java
+++ spring-ai/src/main/java/com/hitesh/springai/controller/OpenAIController.java
@@ -1,4 +1,40 @@
-package com.hitesh.springai;
+package com.hitesh.springai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class OpenAIController {
+
+//    private OpenAiChatModel chatModel;
+
+//    public OpenAIController(OpenAiChatModel chatModel) {
+//        this.chatModel = chatModel;
+//    }
+
+    private ChatClient chatClient;
+
+    public OpenAIController(OpenAiChatModel model) {
+        this.chatClient = ChatClient.create(model);
+    }
+
+    @GetMapping("/chat")
+    public String getAnswer(@RequestParam("query") String query) {
+
+//      return chatModel.call(query);
+
+        ChatResponse chatResponse = chatClient.prompt(query).call().chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response = chatResponse.getResult().getOutput().getText();
+
+        return response;
+    }
 }
